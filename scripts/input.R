2736a070ebf46cf8b1a2d20d0ec664274474e7b8
Felipe Figueiredo <philsf79@gmail.com>
2021-08-23 20:18:35 -0300

    variable selection from "surgical data most recent revision ACLR" section

--- scripts/input.R
+++ scripts/input.R
@@ -9,16 +9,43 @@ library(tidyverse)
 # raw.data <- readxl::read_excel("dataset/file.xls")
 
 # raw.data <- data.table(raw.data)
-dat <- read_excel("dataset/REVISION_ACL_DEIDENTIFIED_MASTER (4) PROMs_final LETvs No LET.xlsx",
+dat.raw <- read_excel("dataset/REVISION_ACL_DEIDENTIFIED_MASTER (4) PROMs_final LETvs No LET.xlsx",
                   sheet = "Clean", skip = 1) %>%
   janitor::clean_names()
 
 # data cleaning -----------------------------------------------------------
 
-dat <- dat %>%
-  select(!ends_with(c("_pri", "_inj", "_pri_inj")))
+dat <- dat.raw %>%
+  # select analytic variables
+  select(!ends_with(c("_pri", "_inj", "_pri_inj"))) %>%
+  select(
+    id,
+    age,
+    sex,
+    bmi,
+    let,
+    stage,
+    graft,
+    graft_diameter,
+    femoral_fixation,
+    tibial_fixation,
+    medial_meniscus,
+    lateral_meniscus,
+    cartilage,
+    mcl,
+    lcl,
+    pcl,
+    bone,
+    implant_hto,
+    femoral_tunnel,
+    tibial_tunnel,
+    )
 
 
 # data wrangling ----------------------------------------------------------
 
-
+dat <- dat %>%
+  # define surgical failure
+  mutate(
+    failure = NA
+  )
