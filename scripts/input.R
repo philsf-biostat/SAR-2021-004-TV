862485300c005e1dd2b63358369dfd3ded000a28
Felipe Figueiredo <philsf79@gmail.com>
2021-08-23 19:54:04 -0300

    minimal input

--- scripts/input.R
+++ scripts/input.R
@@ -1,7 +1,7 @@
 # setup -------------------------------------------------------------------
 library(readxl)
-library(data.table)
-# library(tidyverse)
+# library(data.table)
+library(tidyverse)
 
 
 # data loading ------------------------------------------------------------
@@ -9,9 +9,14 @@ library(data.table)
 # raw.data <- readxl::read_excel("dataset/file.xls")
 
 # raw.data <- data.table(raw.data)
+dat <- read_excel("dataset/REVISION_ACL_DEIDENTIFIED_MASTER (4) PROMs_final LETvs No LET.xlsx",
+                  sheet = "Clean", skip = 1) %>%
+  janitor::clean_names()
 
 # data cleaning -----------------------------------------------------------
 
+dat <- dat %>%
+  select(!ends_with(c("_pri", "_inj", "_pri_inj")))
 
 
 # data wrangling ----------------------------------------------------------
