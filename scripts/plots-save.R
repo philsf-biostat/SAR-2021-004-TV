c3d916d7677fc83e06c2201f2d7cf643a737ddc7
Felipe Figueiredo <philsf79@gmail.com>
2021-08-24 07:04:31 -0300

    NEW: save formatted table in RTF
    We can now substitute the report table from this one, with minimal
    hassle, and keep the table footer as intended! Yay!

--- scripts/plots-save.R
+++ scripts/plots-save.R
@@ -3,4 +3,7 @@
 
 # save plots --------------------------------------------------------------
 
+# table
+inf_baseline %>% as_gt() %>% gt::as_rtf() %>% writeLines(con = "report/SAR-2021-004-TV-v01-T1.rtf")
+
 # ggsave(filename = "figures/plot.png", plot = gg)
