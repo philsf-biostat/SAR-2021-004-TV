ff27abfda4481864b96daa3fac4ba7842b6c7bc8
Felipe Figueiredo <philsf79@gmail.com>
2021-08-25 03:05:29 -0300

    interpret Cohen's D

--- scripts/describe.R
+++ scripts/describe.R
@@ -17,6 +17,13 @@ data.raw %>%
   transmute(dos_pri, dos, fu = as.duration(interval(dos_pri, dos)) ) %>%
   summary()
 
+## interpret cohenD effect sizes
+
+effectsize::interpret_d(.32)
+effectsize::interpret_d(.20)
+effectsize::interpret_d(.04)
+effectsize::interpret_d(.68)
+
 # tables ------------------------------------------------------------------
 
 
